c481679ec1b5caeef0cac009a4dd20926acd679c
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2025-07-04 15:03:35 -0500

    Attempting to create a non-geographical map

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 // Map setup
-var map = L.map('map').setView([51.505, -0.09], 13);
-L.tilelayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-// Marker creation
-var marker = L.marker([51.5, -0.09]).addTo(map);
-
-// Circle creation
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
+var map = L.map('map', {
+    crs: L.CRS.Simple
+});
+var bounds = [[0,0], [1000,1000]];
+var image = L.imageOverlay('uqm_map_full.png', bounds).addTo(map);
+map.fitBounds(bounds);
