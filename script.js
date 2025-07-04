6da75dc3563166a48c55c624aa79d9d124732ccd
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2025-07-04 15:40:34 -0500

    Customized map to look like grass; added favicon

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // Non-geographic map setup
 const map = L.map('map', {
-    crs: L.CRS.Simple,
-    //minZoom: -5
+    crs: L.CRS.Simple
 });
 var  bounds = [[0,0], [1000,1000]];
-var image = L.imageOverlay('assets/img/uqm_map_full.png', bounds).addTo(map);
+var image = L.imageOverlay('assets/img/grass-bkgd.png', bounds).addTo(map);
 map.fitBounds(bounds);
