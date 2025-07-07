2353bd276649131f7480d5557ebbe494dbeeb028
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2025-07-07 16:45:33 -0500

    Created strucutre for DIY tutorial DB

---
+++ src/hardwareStore.js
@@ -0,0 +1,75 @@
+/*** DIY TUTORIALS ***/
+
+// Tutorial DB
+const allTutorials = {
+    lifeHacks: {
+        title: "Life Hacks",
+        tutorials: [
+            {
+                name: "",
+                difficulty: "",
+                time: "",
+                cost: "",
+                materials: [""],
+                instructions: [""],
+                safetyTips: [""],
+                troubleshooting: [""]
+            },
+            {
+                name: "",
+                difficulty: "",
+                time: "",
+                cost: "",
+                materials: [""],
+                instructions: [""],
+                safetyTips: [""],
+                troubleshoting: [""]
+            },
+            {
+                name: "",
+                difficulty: "",
+                time: "",
+                cost: "",
+                materials: [""],
+                instructions: [""],
+                safetyTips: [""],
+                troubleshooting: [""]
+            }
+        ]
+    },
+    crafts: {
+        title: "Crafts",
+        tutorials: [
+            {
+                name: "",
+                difficulty: "",
+                time: "",
+                cost: "",
+                materials: [""],
+                instructions: [""],
+                safetyTips: [""],
+                troubleshooting: [""]
+            },
+            {
+                name: "",
+                difficulty: "",
+                time: "",
+                cost: "",
+                materials: [""],
+                instructions: [""],
+                safetyTips: [""],
+                troubleshooting: [""]
+            },
+            {
+                name: "",
+                difficulty: "",
+                time: "",
+                cost: "",
+                materials: [""],
+                instructions: [""],
+                safetyTips: [""],
+                troubleshooting: [""]
+            }
+        ]
+    }
+};
