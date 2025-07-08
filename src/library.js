bd6484af7add15567bf0303db022ee494cc4ecea
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2025-07-07 23:51:15 -0500

    Created structure for book DB

---
+++ src/library.js
@@ -0,0 +1,205 @@
+/*** FREE BOOKS ***/
+
+// Book DB
+const allBooks = {
+    sciFi: {
+        heading: "Sci-Fi",
+        books: [
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            }
+        ]
+    },
+    fantasy: {
+        heading: "Fantasy",
+        books: [
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            }
+        ]
+    },
+    mystery: {
+        heading: "Mystery",
+        books: [
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            }
+        ]
+    },
+    humor: {
+        heading: "Humor",
+        books: [
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            }
+        ]
+    },
+    plays: {
+        heading: "Plays",
+        books: [
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            }
+        ]
+    },
+    society: {
+        heading: "Society",
+        books: [
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            }
+        ]
+    },
+    travel: {
+        heading: "Travel",
+        books: [
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            }
+        ]
+    },
+    children: {
+        heading: "Children",
+        books: [
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            }
+        ]
+    },
+    poetry: {
+        heading: "Poetry",
+        books: [
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            }
+        ]
+    },
+    biographies: {
+        heading: "Biographies",
+        books: [
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            },
+            {
+                title: "",
+                blurb: "",
+                folder: ""
+            }
+        ]
+    }
+}
